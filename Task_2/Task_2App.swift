f437344850ad543b718567020f070352d3c06a8f
t4rn1337 <80582349+t4rn1337@users.noreply.github.com>
2022-05-22 23:47:13 +0300

    First version for fix

---
+++ Task_2/Task_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Task_2App.swift
+//  Task_2
+//
+//  Created by admin on 22.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Task_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
